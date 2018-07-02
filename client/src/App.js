0754edc7e4af0e38dee29404ac6ed65b9a18551a
Usman Javed <ujaved931@gmail.com>
2018-07-03 03:30:33 +0500

    Troubleshooting

--- client/src/App.js
+++ client/src/App.js
@@ -12,33 +12,38 @@ const API_KEY = "14e1a6731b0a11f74764c3459d833e9b";
 
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
       this.state = {
-        getWeather: []
+        getWeather: [],
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined
     }
   }
-  // componentDidMount() {
-  //   axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`)
-  //     .then(response => {
-  //       console.log(response)
-  //       return response.json();
-  //   })
-  //     .then(data => {
-  //       this.setState({ getWeather: data.results }) 
-  //     })
-  //     .catch(err => {
-        
-  //     })
-  // } 
-  // }
+  componentDidMount() {
+  console.log(this.state)
+  }
+  
+    
   getWeather = async (e) => {
+    console.log(this.state);    
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
     console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+    })
+    console.log(this.state);        
   }
 
   render() {
@@ -46,7 +51,13 @@ class App extends Component {
       <div className="App">
       <Route exact path ='/' component={Header}/>
       <Route exact path ='/' component = {(props) => <Form {...props} getWeather = {this.getWeather} />}/>
-      <Route exact path ='/result' component={Weather} />
+      <Route exact path ='/result' component = {(props) => <Weather {...props} 
+      temperature={this.state.temperature} 
+      city={this.state.city} 
+      country={this.state.country} 
+      humidity={this.state.humidity} 
+      description={this.state.description}  
+      />}/>
       </div>
     );
   }
--- client/src/components/Weather.js
+++ client/src/components/Weather.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 
 
 
+
 class Weather extends Component {
-    state = {
-        temperature: '',
-        windSpeed: '',
-        humidity: '',
-        rain: ''
-    }
     render() {
-    return (
-        <h1> Umbrella App </h1>
-    )
+        return (
+            <div>
+                <p>Temperature: {this.props.temperature}</p>
+                <p>City: {this.props.city}</p>
+                <p>Country: {this.props.country}</p>
+                <p>Humidity: {this.props.humidity}</p>
+                <p>Description: {this.props.description}</p>
+            </div>
+        )
     }
 }
 
