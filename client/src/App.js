027d8dc79fe259e42b663cb6f014141f54f0f927
Usman Javed <ujaved931@gmail.com>
2018-07-03 02:46:08 +0500

    Added router and Weather.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,52 @@
 import React, { Component } from 'react';
+// import axios from 'axios'
+import { Route } from 'react-router'
 import Form from './components/Form';
 import Header from './components/Header';
-import Result from './components/Result';
+import Weather from './components/Weather';
+
+// import Result from './components/Result';
 import './App.css';
 
+const API_KEY = "14e1a6731b0a11f74764c3459d833e9b";
+
+
 class App extends Component {
+  constructor() {
+    super();
+      this.state = {
+        getWeather: []
+    }
+  }
+  // componentDidMount() {
+  //   axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`)
+  //     .then(response => {
+  //       console.log(response)
+  //       return response.json();
+  //   })
+  //     .then(data => {
+  //       this.setState({ getWeather: data.results }) 
+  //     })
+  //     .catch(err => {
+        
+  //     })
+  // } 
+  // }
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+    console.log(data);
+  }
+
   render() {
     return (
       <div className="App">
-        <Header />
-        <Form />
-        <Result />
+      <Route exact path ='/' component={Header}/>
+      <Route exact path ='/' component = {(props) => <Form {...props} getWeather = {this.getWeather} />}/>
+      <Route exact path ='/result' component={Weather} />
       </div>
     );
   }
--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,54 +1,71 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom'
 
 
 class Form extends Component {
     state = {
         country: '',
         city: '',
-        zipcode: ''
-    }
 
+        Weather: []
+    }
 
     render() {
         return (
             <div>
-                <form onSubmit={this.submitHandler}>
-                <label>Country:</label>
-                <input
-                value={this.state.country}
-                type="text"
-                name="country"
-                onChange={this.inputChangeHandler}
-                />
-                <br/>
-                <label>City:</label>
-                <input
-                value={this.state.city}
-                type="text"
-                name="city"
-                onChange={this.inputChangeHandler}
-                />
-                <br/>
-                <label>Zipcode:</label>
-                <input
-                value={this.state.zipcode}
-                type="text"
-                name="zipcode"
-                onChange={this.inputChangeHandler}
-                />
-                <br/>
-                <button type="submit">Get weather</button>
+                <form onSubmit={this.props.getWeather}>
+                    <label>Country:</label>
+                    <input
+                        value={this.state.country}
+                        type="text"
+                        name="country"
+                        onChange={this.inputChangeHandler}
+                    />
+                    <br />
+                    <label>City:</label>
+                    <input
+                        value={this.state.city}
+                        type="text"
+                        name="city"
+                        onChange={this.inputChangeHandler}
+                    />
+                    <br />
+                    <label>Zipcode:</label>
+                    <input
+                        value={this.state.zipcode}
+                        type="text"
+                        name="zipcode"
+                        onChange={this.inputChangeHandler}
+                    />
+                    <br />
+                    <div>
+                        <nav>
+                            <Link to={'/result'}>
+                                Get weather
+                            </Link>
+                        </nav>
+                    </div>
                 </form>
             </div>
         )
     }
-    submitHandler = e => {
-        alert("check the weather")
-        e.preventDefault();
-        console.log(this.state.country)
 
-    };
+    submitHandler = e => {
+        const results = this.state.getWeather
+        const newCity = {
+            country: '',
+            city: '',
+
+        }
+        results.push(newCity)
+        this.props.getWeather(results)
+        this.setState({
+            country: '',
+            city: '',
+
+        })
+    }
+
     inputChangeHandler = e => {
         const { name, value } = e.target;
 
