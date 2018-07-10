f449f682e4bbdb78719272562c48e8a33106ce90
unknown <bonifacemtk15@yahoo.com>
2018-07-10 19:02:07 -0400

    wip

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Weather(models.Model):
+   
+    Temperature = models.CharField(max_length = 200)
+    Location = models.CharField(max_length = 200)
+    Humidity = models.CharField(max_length = 200)
+    description = models.CharField(max_length = 200)
+    createdAt  = models.DateTimeField(auto_now_add= True)
+    last_modified =models.DateTimeField(auto_now_add= True)
+    category = models.CharField(max_length=20)
