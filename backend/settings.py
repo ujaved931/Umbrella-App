15b5fd94de412834648e2e6d6e134ef8292f645f
unknown <bonifacemtk15@yahoo.com>
2018-07-11 19:13:17 -0400

    solved the issue

--- backend/settings.py
+++ backend/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = config('DEBUG', default = False, cast=bool)
-DEBUG = True
-ALLOWED_HOSTS = [ '*']
+DEBUG = config('DEBUG', default = False, cast=bool)
+
+ALLOWED_HOSTS = [ 'localhost', '127.0.0.1']
 
 # Application definition
 
--- weather/models.py
+++ weather/models.py
@@ -1,14 +1,12 @@
 from django.db import models
-
+from uuid import uuid4
 # Create your models here.
-"""
 class Weather(models.Model):
-   
-    Temperature = models.CharField(max_length = 200)
-    Location = models.CharField(max_length = 200)
-    Humidity = models.CharField(max_length = 200)
-    description = models.CharField(max_length = 200)
+    id = models.UUIDField(primary_key = True, default = uuid4, editable = False)
+    Temperature = models.CharField(max_length = 50)
+    Location = models.CharField(max_length = 150)
+    Humidity = models.CharField(max_length = 50)
+    description = models.CharField(max_length = 50)
     createdAt  = models.DateTimeField(auto_now_add= True)
     last_modified =models.DateTimeField(auto_now_add= True)
     category = models.CharField(max_length=20)
-"""
